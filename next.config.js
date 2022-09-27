8766d0a0fa7a8229495ee319140e77d660332287
ninja-1337 <evangelou.e.95@gmail.com>
2022-09-28 00:44:11 +0300

    add profile picture

--- next.config.js
+++ next.config.js
@@ -5,7 +5,23 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
   // next.js config
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'https://lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/',
+      },
 
+      {
+        protocol: 'https',
+        hostname: 'https://cdn.discordapp.com',
+        port: '',
+        pathname: '/avatars/',
+      },
+    ],
+  },
   dest: "public",
   register: true,
   skipWaiting: true,
