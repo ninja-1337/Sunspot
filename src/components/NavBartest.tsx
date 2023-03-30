d711c28b0f4f0cfeda696e757d17cc8a385a0ee7
Your Name <you@example.com>
2023-03-30 16:46:33 +0300

    Navbar improvements

--- src/components/NavBartest.tsx
+++ src/components/NavBartest.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import Image from "next/image";
@@ -11,10 +13,14 @@ import souvla from "/images/xsushi-sign.png";
 import Link from "next/link";
 import { Navbar, Text, Avatar, Dropdown, Input } from "@nextui-org/react";
 import { Spacer } from "@nextui-org/react";
-
+import { useState, useRef } from "react";
 function NavBar() {
   const { data: session, status } = useSession();
-
+  const navbarToggleRef = useRef();
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const HandleSideMenu = () => {
+    isSideMenuOpen && navbarToggleRef.current.click();
+  };
   const collapseItems = [
     "Profile",
     "Dashboard",
@@ -23,8 +29,12 @@ function NavBar() {
     "Help & Feedback",
   ];
   return (
-    <Navbar isBordered variant="sticky">
-      <Navbar.Toggle showIn="xs" />
+    <Navbar className="from-[#0b3bd6] to-[#f8a221]">
+      <Navbar.Toggle
+        ref={navbarToggleRef}
+        onChange={(isSelected: boolean) => setIsSideMenuOpen(isSelected)}
+        showIn="xs"
+      />
       <Navbar.Brand
         css={{
           "@xs": {
@@ -34,7 +44,7 @@ function NavBar() {
       >
         <Link href="/">
           <Text b color="inherit" hideIn="xs">
-            Folio
+            SunSpot
           </Text>
         </Link>
       </Navbar.Brand>
@@ -44,15 +54,13 @@ function NavBar() {
         hideIn="xs"
         variant="highlight"
       >
-        <Link color="secondary" href="projects">
-          Projects
+        <Link color="secondary" href="/book">
+          App
         </Link>
         <Spacer />
-        <Link href="/contact">Contact Me</Link>
+        <Link href="/contact">Contact Us</Link>
         <Spacer />
-        <Link href="/stripe">Buy Me A Coffee</Link>
-        <Spacer />
-        <Link href="/guestbook">Guestbook</Link>
+        <Link href="/guestbook">Reviews</Link>
         <Spacer />
         <Link href="/about">About</Link>
       </Navbar.Content>
@@ -79,7 +87,9 @@ function NavBar() {
                 </Dropdown.Trigger>
               )}
               {!session?.user?.image && (
-                <Dropdown.Button>Login</Dropdown.Button>
+                <Dropdown.Button>
+                  <p className="text-l">Login</p>
+                </Dropdown.Button>
               )}
             </>
           </Navbar.Item>
@@ -112,7 +122,7 @@ function NavBar() {
               </Dropdown.Item>
 
               <Dropdown.Item key="analytics" withDivider>
-                <Link href="/friends">My Friends</Link>
+                <Link href="/chats">Saved Chats</Link>
               </Dropdown.Item>
 
               <Dropdown.Item key="logout" withDivider color="error">
@@ -154,28 +164,36 @@ function NavBar() {
       </Navbar.Content>
       <Navbar.Collapse disableAnimation>
         <Navbar.CollapseItem key="About" activeColor="warning">
-          <Link color="inherit" href="/projects">
-            Projects
+          <Link color="inherit" href="/book" onClick={() => HandleSideMenu()}>
+            App
           </Link>
         </Navbar.CollapseItem>
         <Navbar.CollapseItem key="About" activeColor="warning">
-          <Link color="inherit" href="/contact">
-            Contact Me
+          <Link
+            color="inherit"
+            href="/contact"
+            onClick={() => HandleSideMenu()}
+          >
+            Contact Us
           </Link>
         </Navbar.CollapseItem>
         <Navbar.CollapseItem key="Projects" activeColor="warning">
-          <Link color="inherit" href="/stripe">
-            Buy me a coffee
+          <Link color="inherit" href="/chats" onClick={() => HandleSideMenu()}>
+            Saved Chats
           </Link>
         </Navbar.CollapseItem>
         <Navbar.CollapseItem key="About" activeColor="warning">
-          <Link color="inherit" href="/about">
+          <Link color="inherit" href="/about" onClick={() => HandleSideMenu()}>
             About
           </Link>
         </Navbar.CollapseItem>
         <Navbar.CollapseItem key="About" activeColor="warning">
-          <Link color="inherit" href="/guestbook">
-            Guests Book
+          <Link
+            color="inherit"
+            href="/guestbook"
+            onClick={() => HandleSideMenu()}
+          >
+            Reviews
           </Link>
         </Navbar.CollapseItem>
       </Navbar.Collapse>
